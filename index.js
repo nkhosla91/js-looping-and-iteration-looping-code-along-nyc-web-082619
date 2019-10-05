d6dec16462a6f56f51788ce98917f64bd14c63fb
github username <github email address>
2019-10-05 16:17:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+import { endianness } from 'os'
+
 // Code your solutions in this file
+let names = ["Ada", "Brendan", "Ali"]
+
+function writeCards(name){
+let names = ["Lisa", "Kaitlin", "Jan"]
+
+    let messages = []
+    let i = 0
+    while (i < 3) {
+     messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+     i ++
+    }
+    return messages
+}
+
+function countdown(number){
+    let i = 0
+    while (i < number + 1 ){
+        let count = number - i
+        console.log(count)
+        i ++
+    }
+        
+}
